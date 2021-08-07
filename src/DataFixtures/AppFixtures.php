b1ce5e10376d03252d501061a809ae955da303f2
mathilde-chaneto <mathilde.chaneto@gmail.com>
2021-08-07 21:13:35 +0200

    manage files and their access

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
+use Nelmio\Alice\Bridge\Symfony;
+
+//use Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $loader = new NativeLoader();
+
+        //importe le fichier de fixtures et récupère les entités générés
+        $entities = $loader->loadFile(__DIR__.'/fixtures.yaml')->getObjects();
+
+        //empile la liste d'objet à enregistrer en BDD
+        foreach ($entities as $entity) {
+            $manager->persist($entity);
+        };
+
+        //enregistre
+        $manager->flush();
+
+     
+    }
+}
